78b9f7733623f342d57172f53db4498bbafe6f77
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-10 19:31:03 -0300

    feat: Adding Pagination to project

---
+++ Backend/Domain/Pagination/PaginationParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Pagination
+{
+    public class PaginationParameters
+    {
+        const int maxPageSize = 100;
+        public int PageNumber { get; set; }
+        private int _pageSize { get; set; } = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
