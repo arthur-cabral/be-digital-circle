e272ff17f6a6935577ae3f1fdbea16d98b9d76bc
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-10 19:32:35 -0300

    chore: Adding DTO to project

---
+++ Backend/Application/DTOs/Tb01DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class Tb01DTO
+    {
+        public int Id { get; set; }
+        public string? ColTexto { get; set; }
+        public DateTime? ColDt { get; set; }
+    }
+}
---
+++ Backend/Application/Mapping/DomainToDTOMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<PaginationParameters, PaginationParametersDTO>().ReverseMap();
+            CreateMap<Tb01, Tb01DTO>().ReverseMap();
+        }
+    }
+}
